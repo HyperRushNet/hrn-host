b98a640f27c58bc1ca53e7c4a4ae876647013d52
HyperRushNet <hyperrushnetwork@gmail.com>
2025-07-09 12:12:11 +0200

    Update playcanvas-helper.js

--- playcanvas-helper.js
+++ playcanvas-helper.js
@@ -31,18 +31,12 @@ class PlayCanvasHelper {
 
     if (this.world) {
       this.app.on('update', dt => {
-        const fixedTimeStep = 1 / 120; // Kleinere timestep = stabieler
+        const fixedTimeStep = 1 / 60;
         this.world.step(fixedTimeStep);
 
         // Sync physics bodies met PlayCanvas entities
         for (const entry of this.physicsObjects) {
           const { entity, body } = entry;
-
-          if (body.sleepState === CANNON.Body.SLEEPING) {
-            // Sla update over als body slaapt voor performance en stabiliteit
-            continue;
-          }
-
           const p = body.position;
           entity.setPosition(p.x, p.y, p.z);
 
@@ -57,18 +51,13 @@ class PlayCanvasHelper {
     this.world = new CANNON.World();
     this.world.gravity.set(0, -9.82, 0);
     this.world.broadphase = new CANNON.NaiveBroadphase();
-    this.world.solver.iterations = 20; // Meer solver iteraties voor stabiliteit
-    this.world.solver.tolerance = 0.001; // Lagere tolerance helpt jitter te voorkomen
+    this.world.solver.iterations = 10;
 
     this.physicsMaterial = new CANNON.Material("defaultMaterial");
-    const contactMaterial = new CANNON.ContactMaterial(
-      this.physicsMaterial,
-      this.physicsMaterial,
-      {
-        friction: 0.4,
-        restitution: 0.3,
-      }
-    );
+    const contactMaterial = new CANNON.ContactMaterial(this.physicsMaterial, this.physicsMaterial, {
+      friction: 0.4,
+      restitution: 0.3,
+    });
     this.world.addContactMaterial(contactMaterial);
   }
 
@@ -115,21 +104,16 @@ class PlayCanvasHelper {
     if (this.world) {
       const halfExtents = new CANNON.Vec3(size[0] / 2, size[1] / 2, size[2] / 2);
       const shape = new CANNON.Box(halfExtents);
-
-      // Optioneel kleine rotatie om “scheef” te laten vallen
-      const quat = new CANNON.Quaternion();
-
       const body = new CANNON.Body({
         mass,
         shape,
         position: new CANNON.Vec3(...position),
-        quaternion: quat,
         material: this.physicsMaterial,
-        linearDamping: 0.05,    // Meer damping tegen jitter
-        angularDamping: 0.05,
-        allowSleep: true,       // Slaap toestaan voor stabiliteit
-        sleepSpeedLimit: 0.1,   // Snelheid onder deze waarde kan slapen
-        sleepTimeLimit: 1       // Na 1 sec slapen als langzaam
+        linearDamping: 0.01,    // Lagere demping voor natuurlijker bewegen
+        angularDamping: 0.01,
+        allowSleep: true,
+        sleepSpeedLimit: 0.1,
+        sleepTimeLimit: 1
       });
       this.world.addBody(body);
 
@@ -147,7 +131,6 @@ class PlayCanvasHelper {
       color = [0.2, 0.2, 0.2],
       name = 'Plane'
     } = params;
-
     const plane = new pc.Entity(name);
     plane.addComponent("model", { type: "box" }); // dunne box als vloer
     plane.setLocalScale(size[0], 0.1, size[1]);
@@ -167,12 +150,10 @@ class PlayCanvasHelper {
         mass: 0,
         shape,
         position: new CANNON.Vec3(...position),
-        quaternion: new CANNON.Quaternion().setFromEuler(
-          ...rotation.map(v => v * Math.PI / 180)
-        ),
+        quaternion: new CANNON.Quaternion().setFromEuler(...rotation.map(v => v * Math.PI / 180)),
         material: this.physicsMaterial,
-        linearDamping: 0.05,
-        angularDamping: 0.05,
+        linearDamping: 0.01,
+        angularDamping: 0.01,
         allowSleep: true,
         sleepSpeedLimit: 0.1,
         sleepTimeLimit: 1
