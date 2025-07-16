1332e235e513786b914d7a1ae0626906cb9601eb
HyperRushNet <hyperrushnetwork@gmail.com>
2025-07-16 21:18:35 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const now = new Date();
   const time = now.toISOString(); // bijv: "2025-07-16T14:20:30.000Z"
 
